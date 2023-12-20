0bba89841aee835c430a6d8c223da289e63487b2
Leonardo <leonardo38361@gmail.com>
2023-12-19 21:32:26 -0300

    uso de Link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,22 +1,27 @@
-import Link from "next/link";
+import { HomeIcon } from '@primer/octicons-react';
+import Link from 'next/link';
+
+const navItems = [
+	{ path: '/about', text: 'About' },
+	{ path: '/pricing', text: 'Pricing' },
+	{ path: '/contact', text: 'Contact' },
+];
 
 export const Navbar = async () => {
-
 	return (
 		<nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
-			<span>Home</span>
+			<Link href='/' className='flex items-center'>
+				<HomeIcon className='mr-2' />
+				<span>Home</span>
+			</Link>
 
 			<div className='flex flex-1'></div>
 
-			<Link className='mr-2' href='/about'>
-				About
-			</Link>
-			<Link className='mr-2' href='/pricing'>
-				Pricing
-			</Link>
-			<Link className='mr-2' href='/contact'>
-				Contact
-			</Link>
+			{navItems.map((navItem) => (
+				<Link key={navItem.path} className='mr-2' href={navItem.path}>
+					{navItem.text}
+				</Link>
+			))}
 		</nav>
 	);
 };
